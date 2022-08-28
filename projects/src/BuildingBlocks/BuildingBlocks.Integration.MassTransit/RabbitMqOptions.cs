b2a77ccab3303a89081f8c4ccead61113bf53f24
Dr. Veillard <97292397+drveillard@users.noreply.github.com>
2022-08-28 08:05:19 -0400

    Add files via upload

---
+++ projects/src/BuildingBlocks/BuildingBlocks.Integration.MassTransit/RabbitMqOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.Integration.MassTransit;
+
+public class RabbitMqOptions
+{
+    public string Host { get; set; } = "localhost";
+    public string UserName { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+    public string ConnectionString => $"amqp://{UserName}:{Password}@{Host}/";
+}
