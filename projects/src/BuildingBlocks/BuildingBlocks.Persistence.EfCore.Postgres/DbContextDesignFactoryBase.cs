ca64e27f4e3f9409c4e5c4fd9a7317c975add98b
Dr. Veillard <97292397+drveillard@users.noreply.github.com>
2022-08-28 08:03:55 -0400

    Add files via upload

---
+++ projects/src/BuildingBlocks/BuildingBlocks.Persistence.EfCore.Postgres/DbContextDesignFactoryBase.cs
@@ -0,0 +1,44 @@
+using BuildingBlocks.Core.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BuildingBlocks.Persistence.EfCore.Postgres;
+
+public abstract class DbContextDesignFactoryBase<TDbContext> : IDesignTimeDbContextFactory<TDbContext>
+    where TDbContext : DbContext
+{
+    private readonly string _connectionStringSection;
+
+    protected DbContextDesignFactoryBase(string connectionStringSection)
+    {
+        _connectionStringSection = connectionStringSection;
+    }
+
+    public TDbContext CreateDbContext(string[] args)
+    {
+        Console.WriteLine($"BaseDirectory: {AppContext.BaseDirectory}");
+
+        var configuration = ConfigurationHelper.GetConfiguration(AppContext.BaseDirectory);
+        var connectionStringSectionValue = configuration.GetValue<string>(_connectionStringSection);
+
+        if (string.IsNullOrWhiteSpace(connectionStringSectionValue))
+        {
+            throw new InvalidOperationException($"Could not find a value for {_connectionStringSection} section.");
+        }
+
+        Console.WriteLine($"ConnectionString  section value is : {connectionStringSectionValue}");
+
+        var optionsBuilder = new DbContextOptionsBuilder<TDbContext>()
+            .UseNpgsql(
+                connectionStringSectionValue,
+                sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(GetType().Assembly.FullName);
+                    sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(30), null);
+                }
+            ).UseSnakeCaseNamingConvention();
+
+        return (TDbContext)Activator.CreateInstance(typeof(TDbContext), optionsBuilder.Options);
+    }
+}
